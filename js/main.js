a2d49192ebfcdbfff4c5aa34acb7552beab42fb8
bogdanov88 <a09061988@gmail.com>
2020-08-06 23:09:57 +0300

    fix: remade map from google to yandex maps

--- js/main.js
+++ js/main.js
@@ -19,3 +19,18 @@ var mySwiper = new Swiper('.swiper-container', {
 
   effect: "coverflow",
 })
+
+
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [55.76, 37.64],
+    zoom: 10
+  }, {
+    searchControlProvider: 'yandex#search'
+  });
+
+}
+
+
