460b7016a21de5cdf84e8021bdc4ef53715f3e22
bogdanov88 <a09061988@gmail.com>
2020-08-08 09:01:51 +0300

    create: remade from yandex map to google map

--- js/main.js
+++ js/main.js
@@ -21,16 +21,6 @@ var mySwiper = new Swiper('.swiper-container', {
 })
 
 
-ymaps.ready(init);
 
-function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [55.76, 37.64],
-    zoom: 10
-  }, {
-    searchControlProvider: 'yandex#search'
-  });
-
-}
 
 
